7c9748eaefef5da2eac8dee0ff2a7787a7b1df98
Boris Loncaric <borisloncaric5@outlook.com>
2025-01-13 23:15:24 +0100

    Dodan kontroler,view i DBset za Zahtjeve za slob. dane

---
+++ Controllers/ZahtjeviController.cs
@@ -0,0 +1,207 @@
+﻿using HR_menager.BazePodataka_demo;
+using HR_menager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace HR_menager.Controllers
+{
+    public class ZahtjeviController : Controller
+    {
+        private readonly AppDBContext _context;
+        public ZahtjeviController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: ZahtjevController
+        public ActionResult Index()
+        {
+        var zahtjevi = _context.Zahtjevi
+        .Include(z => z.Podnositelj) // Učitaj ime i prezime podnositelja
+        .Include(z => z.ObradioZaposlenik)
+        .Include(z=>z.StatusZahtjeva)// Učitaj ime i prezime osobe koja je obradila zahtjev
+        .ToList();
+            return View(zahtjevi);
+        }
+
+        // GET: ZahtjevController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ZahtjevController/Create
+        public ActionResult Create()
+        {
+            var Zaposlenici = _context.Zaposlenici
+              .Select(z => new { z.Id, ImePrezime = z.VratiImePrezime() })
+              .ToList();
+
+            var StatusiZahtjeva = _context.StatusiZahtjeva
+                .Select(s => new { s.Id, s.Status })
+                .ToList();
+
+            ViewBag.Zaposlenici = new SelectList(Zaposlenici, "Id", "ImePrezime");
+            ViewBag.StatusiZahtjeva = new SelectList(StatusiZahtjeva, "Id", "Status");
+            return View();
+        }
+
+        // GET: ZahtjevController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var zahtjev = _context.Zahtjevi
+             .Include(z => z.Podnositelj)
+             .Include(z => z.StatusZahtjeva)
+             .Include(z => z.ObradioZaposlenik)
+             .FirstOrDefault(z => z.Id == id);
+
+            if (zahtjev == null)
+            {
+                return NotFound();
+            }
+          
+            var Zaposlenici = _context.Zaposlenici
+                .Select(z => new { z.Id, ImePrezime = z.VratiImePrezime() })
+                .ToList();
+
+            var StatusiZahtjeva = _context.StatusiZahtjeva
+                .Select(s => new { s.Id, s.Status })
+                .ToList();
+
+            ViewBag.Zaposlenici = new SelectList(Zaposlenici, "Id", "ImePrezime");
+            ViewBag.StatusiZahtjeva = new SelectList(StatusiZahtjeva, "Id", "Status");
+            return View(zahtjev);
+        }
+
+        // GET: ZahtjevController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var zahtjev = _context.Zahtjevi
+              .Include(z => z.Podnositelj)
+              .ThenInclude(z=>z.Radnomjesto)
+              .ThenInclude(z=>z.Odjel)
+              .Include(z => z.StatusZahtjeva)
+              .Include(z => z.ObradioZaposlenik)
+              .FirstOrDefault(z => z.Id == id);
+
+            if (zahtjev == null)
+            {
+                return NotFound();
+            }
+            return View(zahtjev);
+        }
+
+
+
+        // POST: ZahtjevController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("PodnositeljId, PocetniDatum, KrajnjiDatum, DatumZahtjeva, Razlog, StatusZahtjevaId,ObradioZaposlenikId")] Zahtjev zahtjev)
+        {
+            if (zahtjev.ObradioZaposlenikId == 0) zahtjev.ObradioZaposlenikId = null;
+          
+            if (ModelState.IsValid)
+            {
+               _context.Zahtjevi.Add(zahtjev);
+               _context.SaveChanges();
+               return RedirectToAction("Index");
+            }
+
+            var Zaposlenici = _context.Zaposlenici
+                .Select(z => new { z.Id, ImePrezime = z.VratiImePrezime() })
+                .ToList();
+
+            var StatusiZahtjeva = _context.StatusiZahtjeva
+                .Select(s => new { s.Id, s.Status })
+                .ToList();
+
+            ViewBag.Zaposlenici = new SelectList(Zaposlenici, "Id", "ImePrezime");
+            ViewBag.StatusiZahtjeva = new SelectList(StatusiZahtjeva, "Id", "Status");
+            return View(zahtjev);
+
+        }
+
+
+        // POST: ZahtjevController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [Bind("Id, PodnositeljId, PocetniDatum, KrajnjiDatum, DatumZahtjeva, Razlog, StatusZahtjevaId,ObradioZaposlenikId")] Zahtjev zahtjev)
+        {
+            if(id != zahtjev.Id) return NotFound();
+            if(zahtjev.ObradioZaposlenikId==0)zahtjev.ObradioZaposlenikId=null;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Update Zaposlenik in the database
+                    _context.Update(zahtjev);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Zaposlenici.Any(z => z.Id == id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Greška kod spremanja");
+                    }
+                }
+
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            var zah = _context.Zahtjevi
+                 .Include(z => z.Podnositelj)
+                 .Include(z => z.StatusZahtjeva)
+                 .Include(z => z.ObradioZaposlenik)
+                 .FirstOrDefault(z => z.Id == id);
+
+
+            var Zaposlenici = _context.Zaposlenici
+                .Select(z => new { z.Id, ImePrezime = z.VratiImePrezime() })
+                .ToList();
+
+            var StatusiZahtjeva = _context.StatusiZahtjeva
+                .Select(s => new { s.Id, s.Status })
+                .ToList();
+
+            ViewBag.Zaposlenici = new SelectList(Zaposlenici, "Id", "ImePrezime");
+            ViewBag.StatusiZahtjeva = new SelectList(StatusiZahtjeva, "Id", "Status");
+            return View(zahtjev);
+
+        }
+
+
+        // POST: ZahtjevController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                var zahtjev = _context.Zahtjevi.FirstOrDefault(z => z.Id == id);
+
+                if (zahtjev == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Zahtjevi.Remove(zahtjev);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Models/Zahtjev.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HR_menager.Models
+{
+    [Table("zahtjev_za_slobodan_dan")]
+    public class Zahtjev
+    {
+       
+        private int id;
+        private Zaposlenik? podnositelj;
+        private int podnositeljId;
+        private DateOnly? datumZahtjeva;
+        private DateOnly? pocetniDatum;
+        private DateOnly? krajnjiDatum;
+        private string razlog;
+        private StatusZahtjeva? statusZahtjeva;
+        private int? statusZahtjevaId;
+        //osoba koja je obradila zahtjev
+        private int? obradioZaposlenikId;
+        private Zaposlenik? obradioZaposlenik;
+
+        [Column("id")]
+        [Display(Name = "Šifra zahtjeva")]
+        public int Id { get => this.id; set { this.id = value; } }
+
+        [ForeignKey("zaposlenik_id")]
+        [Column("zaposlenik_id")]
+        [Display(Name ="Podnositelj zahtjeva")]
+        //[Required(ErrorMessage ="Podnositelj zahtjeva je obavezan")]
+        public int PodnositeljId { get => this.podnositeljId;  set { this.podnositeljId = value; } }
+
+        public Zaposlenik? Podnositelj { get => this.podnositelj; set { this.podnositelj = value; } }
+
+        [Column("datum_zahtjeva")]
+        [Display(Name ="Datum podnošenja")]
+        [Required(ErrorMessage ="Datum podnošenja zahtjeva je obavezan")]
+        public DateOnly? DatumZahtjeva { get => this.datumZahtjeva;  set { this.datumZahtjeva = value; } }
+
+
+        [Column("pocetni_datum")]
+        [Display(Name ="Početni datum")]
+        [Required(ErrorMessage ="Početni datum je obavezan")]
+        public DateOnly? PocetniDatum { get => this.pocetniDatum;  set { this.pocetniDatum = value; } }
+
+        [Column("krajnji_datum")]
+        [Display(Name ="Datum završetka")]
+        [Required(ErrorMessage ="Datum završetka je obavezan")]
+        public DateOnly? KrajnjiDatum { get => this.krajnjiDatum;  set { this.krajnjiDatum = value; } }
+
+        [Column("razlog")]
+        [Display(Name ="Razlog")]
+        [Required(ErrorMessage ="Razlog je obavezan")]
+        public string Razlog { get => this.razlog;  set { this.razlog = value; } }
+
+        [ForeignKey("status_zahtjeva")]
+        [Column("status_zahtjeva")]
+        [Display(Name ="Status zahtjeva")]
+        [Range( 1, Int32.MaxValue, ErrorMessage = "Izaberite status zahtjeva")]
+        public int? StatusZahtjevaId { get => this.statusZahtjevaId;  set { this.statusZahtjevaId = value; } }
+
+        public StatusZahtjeva? StatusZahtjeva { get => this.statusZahtjeva; set { this.statusZahtjeva = value; } }
+
+        [ForeignKey("obradio_zaposlenik_id")]
+        [Column("obradio_zaposlenik_id")]
+       
+        public int? ObradioZaposlenikId { get => this.obradioZaposlenikId; set { this.obradioZaposlenikId = value; } }
+
+        [Display(Name ="Zaposlenik koji je obradio zahtjev")]
+        public Zaposlenik? ObradioZaposlenik { get => this.obradioZaposlenik; set { this.obradioZaposlenik = value; } }
+       
+
+     
+    }
+}
